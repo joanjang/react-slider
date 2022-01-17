5acfae48ea7b1f95d71efcd3168804c68cbeb345
je0489 <je0489@naver.com>
2022-01-17 22:10:17 +0900

    Docs: Add banner images and its infomations file

---
+++ src/components/Slider/meta.js
@@ -0,0 +1,49 @@
+const meta = [
+  {
+    filename: "0b775035.jpeg",
+    title: "마케팅 주니어를 찾습니다",
+    desc: "1월 17일(월) 설명회 신청하기",
+  },
+  {
+    filename: "41f7b36e.jpeg",
+    title: "개발자 되고싶은 분들!?",
+    desc: "프론트엔드 무료 교육과정 참여하기",
+  },
+  {
+    filename: "0d36f0b5.jpeg",
+    title: "포트폴리오를 부탁해!",
+    desc: "디자이너의 포폴 살펴보기",
+  },
+  {
+    filename: "3df61cbc.jpeg",
+    title: "해, 커리어 EP 02 공개",
+    desc: "마지막 관문 2라운드의 승자는?",
+  },
+  {
+    filename: "312a0c29.jpeg",
+    title: "2022년 달라지는 노동법령",
+    desc: "노무관리 쟁점 한 눈에 파악하기",
+  },
+  {
+    filename: "baa54448.jpeg",
+    title: "UX 디자이너의 커리어 설계",
+    desc: "브랜드 가치를 더하는 디자인",
+  },
+  {
+    filename: "619f3af7.jpeg",
+    title: "개발자 성장 비결 공개!",
+    desc: "Velog, 글 쓰는 개발자들의 이야기",
+  },
+  {
+    filename: "b2853456.jpeg",
+    title: "성장하는 개발자가 되려면?",
+    desc: "OOO 검색하지 말 것!",
+  },
+  {
+    filename: "fba2df30.jpeg",
+    title: "성과를 내는 마케팅",
+    desc: "실제 사례를 공개합니다!",
+  },
+];
+
+export default meta;
